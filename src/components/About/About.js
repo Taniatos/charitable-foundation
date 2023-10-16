d683154c54ffda436ebbe1f7c3504a9cde169e9d
tania <tanakorchynska@gmail.com>
2023-10-16 14:40:22 -0400

    Changed the grid system

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,26 +4,32 @@ import "./About.css";
 export default function MainScreen() {
   return (
     <section className="about-page">
-      <div className="wrapper grid-container">
-        <div class="about-text">
-          <p>
-            Благодійний фонд імені Андрія Квятковського створено 2022 року...
-            Lorem ipsum dolor sit amet. Ex consequuntur praesentium qui expedita
-            asperiores in facilis facere. Sit eaque pariatur est voluptatem
-            nulla a molestiae voluptatum cum eveniet placeat molestiae
-            voluptatum cum eveniet placeat.eveniet placeat molestiae voluptatum
-            cum eveniet placeat.
-          </p>
-        </div>
-        <div class="about-text">
-          <p>
-            Sit eaque pariatur est voluptatem nulla a molestiae voluptatum cum
-            eveniet placeat. Et explicabo similique sed fugit quos non sunt
-            velit aut obcaecati voluptas sed alias corporis aut quia tempora rem
-            suscipit quos. Et explicabo similique sed fugit quos non sunt velit
-            aut obcaecati voluptas sed alias corporis aut quia tempora rem
-            suscipit quos.
-          </p>
+      <div className="container wrapper">
+        <div className="row">
+          <div className="col-md">
+            <div class="about-text">
+              <p>
+                Благодійний фонд імені Андрія Квятковського створено 2022 року...
+                Lorem ipsum dolor sit amet. Ex consequuntur praesentium qui
+                expedita asperiores in facilis facere. Sit eaque pariatur est
+                voluptatem nulla a molestiae voluptatum cum eveniet placeat
+                molestiae voluptatum cum eveniet placeat.eveniet placeat
+                molestiae voluptatum cum eveniet placeat.
+              </p>
+            </div>
+          </div>
+          <div className="col-md">
+            <div class="about-text">
+              <p>
+                Sit eaque pariatur est voluptatem nulla a molestiae voluptatum
+                cum eveniet placeat. Et explicabo similique sed fugit quos non
+                sunt velit aut obcaecati voluptas sed alias corporis aut quia
+                tempora rem suscipit quos. Et explicabo similique sed fugit quos
+                non sunt velit aut obcaecati voluptas sed alias corporis aut
+                quia tempora rem suscipit quos.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
