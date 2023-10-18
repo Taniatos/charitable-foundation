9d7e1a704378c76f7409795be5faa7595a9e2d2d
tania <tanakorchynska@gmail.com>
2023-10-17 23:29:15 -0400

    Created a Numbers component

---
+++ src/components/Numbers/Numbers.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Numbers.css";
+
+export default function Numbers() {
+  return (
+    <section className="numbers-page">
+      <div className="wrapper numbers-wrapper">
+        <div className="row">
+          <div className="col-sm">
+            <p className="number-each">hello</p>
+          </div>
+          <div className="col-sm">
+            <p className="number-each">hello</p>
+          </div>
+          <div className="col-sm">
+            <p className="number-each">hello</p>
+          </div>
+          <div className="col-sm">
+            <p className="number-each">hello</p>
+          </div>
+          <div className="col-sm">
+            <p className="number-each">hello</p>
+          </div>
+          <div className="col-sm">
+            <p className="number-each">hello</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
