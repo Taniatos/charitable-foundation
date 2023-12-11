436f3481c0adeaebb820ba391b2fee15baa99fe0
tania <tanakorchynska@gmail.com>
2023-12-11 16:39:53 -0500

    Update Work.js
    Added language context

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,49 +1,93 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Work.css";
+import { LanguageContext } from "../Navigation/LanguageContext";
 
 export default function Work() {
+  const { language } = useContext(LanguageContext);
+
+  const content = {
+    UKR: {
+      header: "Робота фонду",
+      sections: [
+        {
+          title: "Секція 1",
+          text: "Lorem ipsum dolor sit amet. Ex consequuntur praesentium qui expedita asperiores in facilis facere. Sit eaque pariatur est voluptatem nulla a molestiae voluptatum cum eveniet placeat molestiae voluptatum cum eveniet placeat.",
+        },
+        {
+          title: "Секція 2",
+          text: "Lorem ipsum dolor sit amet.Ex consequuntur praesentium qui expedita asperiores in facilis facere. Sit eaque pariatur est voluptatem nulla a molestiae voluptatum cum eveniet placeat molestiae voluptatum cum eveniet placeat.",
+        },
+        {
+          title: "Секція 3",
+          text: "Lorem ipsum dolor sit amet.Ex consequuntur praesentium qui expedita asperiores in facilis facere. Sit eaque pariatur est voluptatem nulla a molestiae voluptatum cum eveniet placeat molestiae voluptatum cum eveniet placeat.",
+        },
+        {
+          title: "Секція 4",
+          text: "Lorem ipsum dolor sit amet.Ex consequuntur praesentium qui expedita asperiores in facilis facere. Sit eaque pariatur est voluptatem nulla a molestiae voluptatum cum eveniet placeat molestiae voluptatum cum eveniet placeat.",
+        },
+      ],
+    },
+    ENG: {
+      header: "What We Do",
+      sections: [
+        {
+          title: "Section 1",
+          text: "Lorem ipsum dolor sit amet.Ex consequuntur praesentium qui expedita asperiores in facilis facere. Sit eaque pariatur est voluptatem nulla a molestiae voluptatum cum eveniet placeat molestiae voluptatum cum eveniet placeat.",
+        },
+        {
+          title: "Section 2",
+          text: "Lorem ipsum dolor sit amet.Ex consequuntur praesentium qui expedita asperiores in facilis facere. Sit eaque pariatur est voluptatem nulla a molestiae voluptatum cum eveniet placeat molestiae voluptatum cum eveniet placeat.",
+        },
+        {
+          title: "Section 3",
+          text: "Lorem ipsum dolor sit amet.Ex consequuntur praesentium qui expedita asperiores in facilis facere. Sit eaque pariatur est voluptatem nulla a molestiae voluptatum cum eveniet placeat molestiae voluptatum cum eveniet placeat.",
+        },
+        {
+          title: "Section 4",
+          text: "Lorem ipsum dolor sit amet.Ex consequuntur praesentium qui expedita asperiores in facilis facere. Sit eaque pariatur est voluptatem nulla a molestiae voluptatum cum eveniet placeat molestiae voluptatum cum eveniet placeat.",
+        },
+      ],
+    },
+  };
+
   return (
     <section className="work-page">
       <div className="wrapper">
-        <h1 className="work-page-header">Робота фонду</h1>
+        <h1 className="work-page-header">{content[language].header}</h1>
         <div className="work-grid-box">
           <div className="grid-top">
             <div className="mini-sections mini-section-one">
-              <h2 className="mini-section-header">Section 1</h2>
+              <h2 className="mini-section-header">
+                {content[language].sections[0].title}
+              </h2>
               <p className="mini-section-descr">
-                Lorem ipsum dolor sit amet. Est voluptatem iure quo dolorem
-                explicabo cum libero asperiores sed corrupti assumenda. Aut sunt
-                cumque non recusandae nihil et neque nesciunt qui perferendis
-                beatae non suscipit omnis qui esse voluptates.
+                {content[language].sections[0].text}
               </p>
             </div>
             <div className="mini-sections mini-section-two">
-              <h2 className="mini-section-header">Section 2</h2>
+              <h2 className="mini-section-header">
+                {content[language].sections[1].title}
+              </h2>
               <p className="mini-section-descr">
-                Lorem ipsum dolor sit amet. Est voluptatem iure quo dolorem
-                explicabo cum libero asperiores sed corrupti assumenda. Aut sunt
-                cumque non recusandae nihil et neque nesciunt qui perferendis
-                beatae non suscipit omnis qui esse voluptates.
+                {content[language].sections[1].text}
               </p>
             </div>
           </div>
           <div className="grid-bottom">
             <div className="mini-sections mini-section-three">
-              <h2 className="mini-section-header">Section 3</h2>
+              <h2 className="mini-section-header">
+                {content[language].sections[2].title}
+              </h2>
               <p className="mini-section-descr">
-                Lorem ipsum dolor sit amet. Est voluptatem iure quo dolorem
-                explicabo cum libero asperiores sed corrupti assumenda. Aut sunt
-                cumque non recusandae nihil et neque nesciunt qui perferendis
-                beatae non suscipit omnis qui esse voluptates.
+                {content[language].sections[2].text}
               </p>
             </div>
             <div className="mini-sections mini-section-four">
-              <h2 className="mini-section-header">Section 4</h2>
+              <h2 className="mini-section-header">
+                {content[language].sections[3].title}
+              </h2>
               <p className="mini-section-descr">
-                Lorem ipsum dolor sit amet. Est voluptatem iure quo dolorem
-                explicabo cum libero asperiores sed corrupti assumenda. Aut sunt
-                cumque non recusandae nihil et neque nesciunt qui perferendis
-                beatae non suscipit omnis qui esse voluptates.
+                {content[language].sections[3].text}
               </p>
             </div>
           </div>
