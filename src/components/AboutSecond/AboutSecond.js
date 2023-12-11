0f687c80dccb258089a049c622089202e3d878ad
tania <tanakorchynska@gmail.com>
2023-12-11 16:09:37 -0500

    Added language context to aboutsecond files

--- src/components/AboutSecond/AboutSecond.js
+++ src/components/AboutSecond/AboutSecond.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./AboutSecond.css";
 import SlideShow from "./SlideShow";
 import AnimatedTitle from "./AnimatedTitle";
+import { LanguageContext } from "../Navigation/LanguageContext";
 
 export default function AboutSecond() {
+  const { language } = useContext(LanguageContext);
+
+  const content = {
+    UKR: {
+      text1:
+        "Lorem ipsum dolor sit amet. Ex consequuntur praesentium qui expedita asperiores in facilis facere. Sit eaque pariatur est voluptatem nulla a molestiae voluptatum cum eveniet placeat molestiae voluptatum cum eveniet placeat.eveniet placeat molestiae voluptatum cum eveniet placeat.Lorem ipsum dolor sit amet. at.eveniet placeat molestiae voluptatum cum eveniet placeat.",
+      text2:
+        "Ex consequuntur praesentium qui expedita asperiores in facilis facere. Sit eaque pariatur est voluptatem nulla a molestiae voluptatum cum eveniet placeat molestiae voluptatum cum eveniet Lorem ipsum dolor sit amet. Ex consequuntur praesentium qui expedita asperiores in facilis facere. Sit eaque pariatur est voluptatem nulla a molestiae voluptatum cum eveniet placeat molestiae voluptatum cum eveniet placeat.eveniet placeat molestiae voluptatum cum eveniet placeat.Lorem ipsum dolor sit amet. at.eveniet placeat molestiae voluptatum cum eveniet placeat.",
+    },
+    ENG: {
+      text1:
+        "Lorem ipsum dolor sit amet. Ex consequuntur praesentium qui expedita asperiores in facilis facere. Sit eaque pariatur est voluptatem nulla a molestiae voluptatum cum eveniet placeat molestiae voluptatum cum eveniet placeat.eveniet placeat molestiae voluptatum cum eveniet placeat.Lorem ipsum dolor sit amet. at.eveniet placeat molestiae voluptatum cum eveniet placeat.",
+      text2:
+        "Ex consequuntur praesentium qui expedita asperiores in facilis facere. Sit eaque pariatur est voluptatem nulla a molestiae voluptatum cum eveniet placeat molestiae voluptatum cum eveniet Lorem ipsum dolor sit amet. Ex consequuntur praesentium qui expedita asperiores in facilis facere. Sit eaque pariatur est voluptatem nulla a molestiae voluptatum cum eveniet placeat molestiae voluptatum cum eveniet placeat.eveniet placeat molestiae voluptatum cum eveniet placeat.Lorem ipsum dolor sit amet. at.eveniet placeat molestiae voluptatum cum eveniet placeat.",
+    },
+  };
+
   return (
     <section className="about-second-page">
       <div className="wrapper">
@@ -12,25 +30,8 @@ export default function AboutSecond() {
         </div>
         <div className="about-second-grid-container">
           <div className="about-second-text">
-            <p>
-              Lorem ipsum dolor sit amet. Ex consequuntur praesentium qui
-              expedita asperiores in facilis facere. Sit eaque pariatur est
-              voluptatem nulla a molestiae voluptatum cum eveniet placeat
-              molestiae voluptatum cum eveniet placeat.eveniet placeat molestiae
-              voluptatum cum eveniet placeat.Lorem ipsum dolor sit amet.
-              at.eveniet placeat molestiae voluptatum cum eveniet placeat.
-            </p>
-            <p>
-              Ex consequuntur praesentium qui expedita asperiores in facilis
-              facere. Sit eaque pariatur est voluptatem nulla a molestiae
-              voluptatum cum eveniet placeat molestiae voluptatum cum eveniet
-              Lorem ipsum dolor sit amet. Ex consequuntur praesentium qui
-              expedita asperiores in facilis facere. Sit eaque pariatur est
-              voluptatem nulla a molestiae voluptatum cum eveniet placeat
-              molestiae voluptatum cum eveniet placeat.eveniet placeat molestiae
-              voluptatum cum eveniet placeat.Lorem ipsum dolor sit amet.
-              at.eveniet placeat molestiae voluptatum cum eveniet placeat.
-            </p>
+            <p>{content[language].text1}</p>
+            <p>{content[language].text2}</p>
           </div>
           <div className="about-second-slideshow">
             <SlideShow />
--- src/components/AboutSecond/AnimatedTitle.js
+++ src/components/AboutSecond/AnimatedTitle.js
@@ -1,21 +1,42 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./AnimatedTitle.scss";
+import { LanguageContext } from "../Navigation/LanguageContext";
+
 export default function AnimatedTitle() {
-  const words = [
-    "військовослужбовець",
-    "батько",
-    "чоловік",
-    "брат",
-    "друг",
-    "патріот"
-  ];
+  const { language } = useContext(LanguageContext);
+
+  const content = {
+    UKR: {
+      title: "АНДРІЙ КВЯТКОВСЬКИЙ:",
+      words: [
+        "військовослужбовець",
+        "батько",
+        "чоловік",
+        "брат",
+        "друг",
+        "патріот",
+      ],
+    },
+    ENG: {
+      title: "ANDRII KVIATKOVSKYI:",
+      words: [
+        "military serviceman",
+        "father",
+        "husband",
+        "brother",
+        "friend",
+        "patriot",
+      ],
+    },
+  };
+
   return (
     <div className="animated-title">
-      <h1 className="animated-title__static">АНДРІЙ КВЯТКОВСЬКИЙ:</h1>
+      <h1 className="animated-title__static">{content[language].title}</h1>
       <div className="animated-title__animated content">
         <div className="content__container">
           <ul className="content__container__list">
-            {words.map((word, index) => (
+            {content[language].words.map((word, index) => (
               <li key={index} className="content__container__list__item">
                 {word}
               </li>
